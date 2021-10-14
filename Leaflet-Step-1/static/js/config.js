6aca7d866863d51b6c2fb6f4cc94f4e2b5be7b52
Darrell S Horich <d11eleven@gmail.com>
2021-10-14 15:07:08 -0500

    incremental save

--- Leaflet-Step-1/static/js/config.js
+++ Leaflet-Step-1/static/js/config.js
@@ -1,4 +1,4 @@
 // API key
 // const API_KEY = "pk.eyJ1IjoiZDExZWxldmVuIiwiYSI6ImNrdWx1cWxidjNsZzYycXFqcXJ5YWJqbmQifQ.xULvTLUnXtGLfNLX1QbKeQ";
-const API_KEY = "pk.eyJ1IjoiZDExZWxldmVuIiwiYSI6ImNrdWx0c2RmZjNrZ3kycXE2cjE3aDZnNDMifQ.bGkooU14KBua97MEwZ-w4g";
+const API_KEY = "pk.eyJ1IjoiZDExZWxldmVuIiwiYSI6ImNrdWx0c2RmZjNrZ3kycXE2cjE3aDZnNDMifQ.bGkooU14KBuxxxxxxxxxx";
 
